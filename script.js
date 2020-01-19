328dd5db1d2790503169f35302c12e27b7802b12
eva <eva.koumartzi@gmail.com>
2020-01-20 00:54:16 +0100

    Update script.js
    Co-Authored-By: mino <minotalen@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let Grid =[];
 let p1Color;
 let p2Color;
 let activePlayer1;
-
+let gameState; //playing, winScreen
 function setup (){
+  gameState = "playing";
   p1Color = color(255,255,0);
   p2Color = color(255,0,0);
   activePlayer1=true;
@@ -24,21 +25,31 @@ function setup (){
 function draw (){
   ellipseMode(CORNER);
   background(220);
-  for (let i=0; i<columns; i++){
-    for(let j=0; j< rows; j++){
-      stroke(200,0,100);
-      strokeWeight(5);
-      fill(50,220,0);
-      rect(i*width/columns, j*height/rows, width/columns, height/rows);
-      if(Grid[i][j] ==1){
-        fill(p1Color);
-        ellipse(i*width/columns, j*height/rows, width/columns, height/rows);
+  switch(gameState){
+    case "playing":
+      for (let i=0; i<columns; i++){
+        for(let j=0; j< rows; j++){
+          stroke(200,0,100);
+          strokeWeight(5);
+          fill(50,220,0);
+          rect(i*width/columns, j*height/rows, width/columns, height/rows);
+          if(Grid[i][j] ==1){
+            fill(p1Color);
+            ellipse(i*width/columns, j*height/rows, width/columns, height/rows);
+          }
+          if(Grid[i][j] ==2){
+            fill(p2Color);
+            ellipse(i*width/columns, j*height/rows, width/columns, height/rows);
+          }
+          if(Grid[i][j] ==3 && Math.floor(millis()/444)%2 == 0){
+            fill(0);
+            ellipse(i*width/columns, j*height/rows, width/columns, height/rows);
+          }
+        }
       }
-      if(Grid[i][j] ==2){
-        fill(p2Color);
-        ellipse(i*width/columns, j*height/rows, width/columns, height/rows);
-      }
-    }
+      break;
+    case "winScreen":
+
   }
 }
 
@@ -60,4 +71,124 @@ function drop(dropCol){
     }
   }
   console.log("all full");
+<<<<<<< Updated upstream
+=======
+
+}
+
+function win(){
+  let active=0;
+  let consecutive=0;
+    //check column wins
+  for (let i=0; i<columns; i++){
+    active=0;
+    consecutive=0;
+    for(let j=0; j< rows; j++){
+      if(Grid[i][j]==active && Grid[i][j]!=0){
+        consecutive++;
+      }else{
+        consecutive=0;
+        active=Grid[i][j];
+      }
+      if(consecutive>=3){
+        console.log("winnner! " + active);
+        Grid[i][j] = 3;
+        Grid[i][j-1] = 3;
+        Grid[i][j-2] = 3;
+        Grid[i][j-3] = 3;
+      }
+    }
+  }
+    //check row wins
+  for (let i=0; i<rows; i++){
+    active=0;
+    consecutive=0;
+    for(let j=0; j< columns; j++){
+      if(Grid[j][i]==active && Grid[j][i]!=0){
+        consecutive++;
+      }else{
+        consecutive=0;
+        active=Grid[j][i];
+      }
+      if(consecutive>=3){
+        console.log("horri winnner! " + active);
+      }
+    }
+  }
+  //check diagonaL wins
+  for (let i=0; i<columns; i++){
+    active=0;
+    consecutive=0;
+    for(let j=0; j< rows; j++){
+      if(i+j < columns){
+        // console.log("checking " + (i+j) + ", " + j);
+        if(Grid[i+j][j]==active && Grid[i+j][j]!=0){
+          consecutive++;
+        }else{
+          consecutive=0;
+          active=Grid[i+j][j];
+        }
+        if(consecutive>=3){
+          console.log("bbb winnner! " + active);
+
+        }
+      }
+    }
+  }
+  for (let i=0; i<rows; i++){
+    active=0;
+    consecutive=0;
+    for(let j=0; j< columns; j++){
+      if(i+j < rows){
+        // console.log("xxx checking " + (j) + ", " + (i+j));
+        if(Grid[j][i+j]==active && Grid[j][i+j]!=0){
+          consecutive++;
+        }else{
+          consecutive=0;
+          active=Grid[j][i+j];
+        }
+        if(consecutive>=3){
+          console.log("diagolan forward winnner! " + active);
+        }
+      }
+    }
+  }
+  //check diagonaL wins
+  for (let i=0; i<columns; i++){
+    active=0;
+    consecutive=0;
+    for(let j=0; j< rows; j++){
+      if(i-j >= 0){
+        // console.log("checking " + (i-j) + ", " + j);
+        if(Grid[i-j][j]==active && Grid[i-j][j]!=0){
+          consecutive++;
+        }else{
+          consecutive=0;
+          active=Grid[i-j][j];
+        }
+        if(consecutive>=3){
+          console.log("winnner! " + active);
+        }
+      }
+    }
+  }
+  for (let i=0; i<rows; i++){
+    active=0;
+    consecutive=0;
+    for(let j=0; j< columns; j++){
+      if(i-j >= 0){
+        // console.log("xxx checking " + (j) + ", " + (i-j));
+        if(Grid[j][i-j]==active && Grid[j][i-j]!=0){
+          consecutive++;
+        }else{
+          consecutive=0;
+          active=Grid[j][i-j];
+        }
+        if(consecutive>=3){
+          console.log("diagolan BACKKKKKK winnner! " + active);
+        }
+      }
+    }
+  }
+>>>>>>> Stashed changes
 }
